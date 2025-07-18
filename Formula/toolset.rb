7861e1535b7fa71d6a233774b9ebcbeead86d066
goreleaserbot <kazhuravlev+goreleaserbot@fastmail.com>
2025-07-18 22:37:39 +0200

    Brew formula update for toolset version v0.27.0

--- Formula/toolset.rb
+++ Formula/toolset.rb
@@ -5,13 +5,13 @@
 class Toolset < Formula
   desc "Toolset helps to keep a project-specific tools"
   homepage "https://github.com/kazhuravlev/toolset"
-  version "0.25.2"
+  version "0.27.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kazhuravlev/toolset/releases/download/v0.25.2/toolset_Darwin_arm64.tar.gz"
-      sha256 "605753616a1dd7aace4567a69c056fd30f0358ebbf0e8ee17bb09dafb7d6357b"
+      url "https://github.com/kazhuravlev/toolset/releases/download/v0.27.0/toolset_Darwin_arm64.tar.gz"
+      sha256 "097039c6d789f0d3887e9e4e2b9ad55f366b6f417ab1ce3bc99a9d2716d1ae12"
 
       def install
         bin.install "toolset"
@@ -20,14 +20,11 @@ class Toolset < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/kazhuravlev/toolset/releases/download/v0.25.2/toolset_Linux_arm64.tar.gz"
-        sha256 "a73189154d851ac52124f5d8535b682339d3c7bf0a1a41b3ad1df3b36d6b5e1e"
-
-        def install
-          bin.install "toolset"
-        end
+    if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
+      url "https://github.com/kazhuravlev/toolset/releases/download/v0.27.0/toolset_Linux_arm64.tar.gz"
+      sha256 "e03abeeacff04e6cb36af9173e557243745c779d1bdd6032701ec6484be6e10e"
+      def install
+        bin.install "toolset"
       end
     end
   end
